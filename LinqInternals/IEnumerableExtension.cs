a4a3af09ad87240cdc21d808e6a0ec2edbaedd5c
Pramil <pramilgawande@gmail.com>
2020-04-29 02:44:25 +0530

    Add project files.

---
+++ LinqInternals/IEnumerableExtension.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinqInternals
+{
+    public static class IEnumerableExtension
+    {
+        public static IEnumerable<T> MyWhere<T>(
+            this IEnumerable<T> items,
+            Func<T, bool> predicate)
+        {
+            //So, How are we going to implement this.
+            //First, we need to loop through the items,
+            //then we need to see if the predicate is successful or not
+            //and if the predicate is successful we need to return the
+            //value out.
+
+            //var list = new List<T>();
+
+            foreach (var item in items)
+            {
+                if (predicate(item))
+                {
+                    //list.Add(item);
+                    yield return item;
+                }
+            }
+
+            //return list;
+
+            //First we'll see how the items are returned in actual WHERE method
+            //Let's run and see if we've successfully implemented our Method.
+
+            //This is what we got in our implementation. 
+            //All the items were returned even before we looped through evenNumbers
+
+            //In actual implementation of WHERE we got the items as an IEnumerable
+            //which we saw in Results View.
+
+            //This is not how we want our WHERE implementation to return the items.
+
+            //So how do we acheive this?
+
+            //Now is the right time to reveal the third constituent. :-)
+
+            //The third main constituent is the keyword 'yield'.
+            //Yield keyword yields the return and then continue.
+        }
+    }
+}
---
+++ LinqInternals/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace LinqInternals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            //var evenNumbers = numbers.Where(x => x % 2 == 0);
+            var evenNumbers = numbers.MyWhere(x => x % 2 == 0);
+            //The execution doesn't happen in the above statement.
+            //WHERE executes when we loop through evenNumbers
+            //or when we do evenNumbers.ToList()
+            //or evenNumbers.ToArray()
+            foreach (var item in evenNumbers)
+            {
+                Console.WriteLine(item);
+            }
+
+            //Let's see if this works. Fingers-Crossed.
+            //See how is the flow of execution.
+            //Check the item variable.
+
+            //So we've successfully implemented our WHERE method.
+        }
+    }
+}
